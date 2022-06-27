627eef481d73ea1ec5318ba124f931e827f681b9
Pedro Figueira <pe.fogueyra@gmail.com>
2022-06-27 16:57:21 -0300

    refatorando navegações por tab

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,22 +1,31 @@
-export default function sectionAnimais() {
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'activeAnimalDescr';
+  }
+
   // SECAO ANIMAIS INTERATIVA
-  const tabMenu = document.querySelectorAll('.js-tabMenu li');
-  const tabContent = document.querySelectorAll('.js-tabContent section');
 
-  function activeTab(index) {
-    tabContent.forEach((limpaSection) => {
-      limpaSection.classList.remove('activeAnimalDescr'); // removendo a classe se ela já tiver sido atribuída a outro elemento da lista
+  activeTab(index) {
+    this.tabContent.forEach((limpaSection) => {
+      limpaSection.classList.remove(this.activeClass);
     });
 
-    tabContent[index].classList.add('activeAnimalDescr'); // adicionando a classe ao elemento clicado
+    this.tabContent[index].classList.add(this.activeClass);
   }
 
-  // estrutura de condicao para ver se as classes chamadas realmente existem
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('activeAnimalDescr'); // adicionando a classe para tornar o conteúdo visível inicialmente
-    tabMenu.forEach((itemMenu, index2) => {
-      itemMenu.addEventListener('click', () => activeTab(index2)); // passando o evento a ser executado após o click e lincando o index das imagens ao index do texto
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index)); // passando o evento a ser executado após o click e lincando o index das imagens ao index do texto
     });
   }
-  // FIM SECAO ANIMAIS INTERATIVA
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
+    return this;
+  }
 }
